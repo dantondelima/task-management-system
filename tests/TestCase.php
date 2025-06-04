b237fd36a12f287a5c20b332e7bc015d7d20148f
Danton de Lima <dantondelima.contato@gmail.com>
2025-06-04 12:32:22 -0300

    chore: adjust pest config to use sqlite

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,9 +4,32 @@ declare(strict_types=1);
 
 namespace Tests;
 
+use Illuminate\Contracts\Console\Kernel;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public function createApplication()
+    {
+        // Forces sqlite for testing
+        putenv('APP_ENV=testing');
+        $_ENV['APP_ENV'] = 'testing';
+        $_SERVER['APP_ENV'] = 'testing';
+
+        $app = require __DIR__.'/../bootstrap/app.php';
+
+        $app->make(Kernel::class)->bootstrap();
+
+        $app['config']->set('database.default', 'sqlite');
+        $app['config']->set('database.connections.sqlite.database', ':memory:');
+
+        return $app;
+    }
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
 }
