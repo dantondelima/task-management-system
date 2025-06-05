d35ea46f4ec65083ce5819c621ac723e98478fe3
Danton de Lima <dantondelima.contato@gmail.com>
2025-06-04 21:25:39 -0300

    feat: add route for categories

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
@@ -26,3 +27,4 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::resource('tasks', TaskController::class)->middleware('auth');
+Route::resource('categories', CategoryController::class)->middleware('auth');
